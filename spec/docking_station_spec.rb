dd696d64ab2643dd33310e16112df383bf8b9897
Henry Cooksley <henry.cooksley@gmail.com>
2018-10-02 14:59:54 +0100

    bike is woke

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+    it 'returns a bike' do
+      expect(subject.release_bike).to be_woke
+    end
+
 end
