dccda3ec2d6ec4d9b42812d2f9be2f1ae6686da3
Henry Cooksley <henry.cooksley@gmail.com>
2018-10-02 16:43:40 +0100

    error raised when docking station empty

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,7 +4,8 @@ class DockingStation
   attr_accessor :current_bike
 
   def release_bike
-    Bike.new
+    raise "No bike available" unless @current_bike
+    @current_bike
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,18 +3,21 @@ require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
     it 'returns a bike' do
+      bike = Bike.new
+      subject.dock(bike)
       expect(subject.release_bike).to be_working
     end
-    # it 'bike docked' do
-    #   expect(subject.dock(Bike.new)).to eq subject.current_bike
-    # end
 
-    it { is_expected.to respond_to :dock }
     it 'look at current bike' do
-      bikey = Bike.new
-      subject.dock(bikey)
-      expect(subject.current_bike).to eq bikey
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.current_bike).to eq bike
+    end
+
+    it 'raises an error when no bikes' do
+      expect { subject.release_bike }.to raise_error("No bike available")
     end
 end
